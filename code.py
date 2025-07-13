fea2af328c31a03cc42a4d572f59b38a6df56e0c
gastrophobic <136510554+gastrophobic@users.noreply.github.com>
2025-07-13 16:53:05 +0300

    Update code.py

--- code.py
+++ code.py
@@ -105,32 +105,9 @@ def run(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display
                     return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 
                 case 3:
-                    while True:
-                        gun_option = get_valid_input(
-                            f"1: {Fore.GREEN}Shared Guns{Style.RESET_ALL}\n"
-                            f"2: {Fore.GREEN}Terrorist Guns{Style.RESET_ALL}\n"
-                            f"3: {Fore.GREEN}Counter-Terrorist Guns{Style.RESET_ALL}\n"
-                            f"Type 'back' to return to Asset replacements menu.\n: ",
-                            valid_values=[1, 2, 3]
-                        )
-
-                        if gun_option == 'back':
-                            print(f"{Fore.CYAN}\nReturning to Asset replacements menu.{Style.RESET_ALL}")
-                            break
-
-                        match gun_option:
-                            case 0:
-                                start_key = "sharedguns"
-                                start_key2 = "replacement sounds"
-                            case 1:
-                                start_key = "counterterroristguns"
-                                start_key2 = "replacement sounds"
-                            case 2:
-                                start_key = "terroristguns"
-                                start_key2 = "replacement sounds"
-
-                        start_key2 = "replacement sounds"
-                        return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                    start_key = "CB Gun Sound"
+                    start_key2 = "replacement sounds"
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 
         except Exception as e:
             print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
